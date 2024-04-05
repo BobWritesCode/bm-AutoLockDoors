f0c5d2f73521ae933d912b28a2befbfa24544b56
BobWritesCode <warwick_hart@hotmail.com>
2024-04-05 09:57:48 +0100

    fix: move cd_doorlock outside of player search

--- server/server.lua
+++ server/server.lua
@@ -11,14 +11,14 @@ function DoorLockThread()
       for k, v in pairs(Countdowns) do
         Countdowns[k].count -= 1
         if Countdowns[k].count <= 0 then
-          local players = QBCore.Functions.GetPlayers()
-          for _, v2 in pairs(players) do
-            if Config.doorLockFramework == 'QB' then
+          if Config.doorLockFramework == 'QB' then
+            local players = QBCore.Functions.GetPlayers()
+            for _, v2 in pairs(players) do
               TriggerEvent('qb-doorlock:server:updateState', k, false, false, false, false, false, false, v2)
-            elseif Config.doorLockFramework == 'cd_doorlock' then
-              TriggerClientEvent('cd_doorlock:SetDoorState_name', -1, false, k, v2.group)
+              break
             end
-            break
+          elseif Config.doorLockFramework == 'cd_doorlock' then
+            TriggerClientEvent('cd_doorlock:SetDoorState_name', -1, false, k, v.group)
           end
           Countdowns[k] = nil
         end
@@ -35,12 +35,12 @@ function LockDoor(source, doorName, doorGroup)
     TriggerEvent('qb-doorlock:server:updateState', doorName, true, false, false, false, false, false, source)
   elseif Config.doorLockFramework == 'cd_doorlock' then
     TriggerClientEvent('cd_doorlock:SetDoorState_name', -1, true, doorName, doorGroup)
-  Notification(source, 'Security lock', 'The door locks have been triggered, your need to wait it out')
+    Notification(source, 'Security lock', 'The door locks have been triggered, your need to wait it out')
   end
   Countdowns[doorName] = {}
   Countdowns[doorName].count = Config.AutoLockingDoords[doorName].lockTime
   Countdowns[doorName].coords = Config.AutoLockingDoords[doorName].coords
-  TriggerClientEvent('bm-AutoLockDoors:client:RecieveCountdowns', -1 , Countdowns)
+  TriggerClientEvent('bm-AutoLockDoors:client:RecieveCountdowns', -1, Countdowns)
 end
 
 RegisterNetEvent('bm-AutoLockDoors:server:lockdoor', function(doorName, doorGroup)
